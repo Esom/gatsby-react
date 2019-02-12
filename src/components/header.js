b868f4b4ddf0a734db60ca178d8184db1a916469
Ekeh Esom <mailesom@gmail.com>
2019-02-12 18:14:11 +0100

    dotenv setup

---
+++ src/components/header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from 'gatsby-link'
+import './Header.css'
+
+class header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if(scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src='/images/logo-designcode.svg' width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default header;
---
+++ src/layouts/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import Header from '../components/header'
+import './index.css'
+import Footer from '../components/Footer';
+
+const Layout = ({ children, data } ) => (
+  <div>
+    {/* helmet takes care of search engine optimization */}
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'keywords', content: data.site.siteMetadata.keywords },
+      ]}
+    />
+    <Header />
+    {children()}
+    <Footer data={data}>
+      Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:support@designcode.io">Email us</a> to ask anything. © 2018
+    </Footer>
+  </div>
+
+)
+
+Layout.propTypes = {
+  children: PropTypes.func,
+}
+
+export default Layout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        keywords
+      }
+    }
+    # allContentfulLink(sort: { fields: [createdAt], order: ASC }) {
+    #   edges {
+    #     node {
+    #       title
+    #       url
+    #       createdAt
+    #     }
+    #   }
+    # }
+  }
+`
