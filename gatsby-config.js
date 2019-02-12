e637b663a1e609c2213c9d4d88698e488892b234
Ekeh Esom <mailesom@gmail.com>
2019-02-12 18:19:37 +0100

    expose gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.spaceId,
+        accessToken: process.env.accessToken
+      }
+    }
+  ],
+}
